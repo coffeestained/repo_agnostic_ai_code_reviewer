be664159620484f3e3c685d704108def0568f703
Matthew Grady <matthewbgrady90@gmail.com>
2025-07-28 23:42:06 -0400

    rebase/secret-rewrite

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import { Core } from './providers/Core.js';
 import 'dotenv/config';
+import { Logger } from './lib/logger.js';
 
 const coreService = new Core();
 
@@ -16,10 +17,12 @@ app.get('/health', (_, res) => res.status(200).send('OK'));
 app.post('/webhooks', async (req, res) => {
     const provider = coreService.parseProviderFromRequest(req);
     if (!provider) throw "Bad Request";
+    Logger.info(`${provider.constructor.name} provider activated`);
+    Logger.info(`${provider.constructor.name} provider activated`, JSON.stringify(req.body, null, 2));
     const pullRequestData = provider.fetchPullRequestData();
     res.status(200).send('Webhook request processed');
 });
 
 app.listen(PORT, () => {
-    console.log(`Reviewer Bot listening on port ${PORT}`);
+    Logger.info(`Reviewer Bot listening on port ${PORT}`);
 });
